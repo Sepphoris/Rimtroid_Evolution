24446a8baf1252648e7860e6925a6d704447bf82
Drazzii <70376013+Drazzii@users.noreply.github.com>
2020-08-28 04:24:22 -0700

    Add files via upload

---
+++ Assemblies/RT_PawnDefs/RT_PawnDefs/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+using RimWorld;
+using System.Collections.Generic;
+
+namespace RT_StartingHediffs
+{
+    public class CompProperties_InitialHediff : CompProperties
+    {
+        public string hediffname;
+        public float hediffseverity;
+        public int numberOfHediffs;
+
+        public CompProperties_InitialHediff()
+        {
+            base.pawn();
+            this.compClass = (__Null)typeof(CompInitialHediff);
+        }
+    }
+}
