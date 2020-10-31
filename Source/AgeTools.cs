ee01039c5a4639b08de77d5bba1f78eca3135518
Drazzii <70376013+Drazzii@users.noreply.github.com>
2020-10-31 10:55:45 -0700

    Tweaked thinktrees and resources
    >Metroids now won't aggro on everything.
    
    >Metroid gel gives a debugg when eaten
    
    >Inertia recipe file removed.
    
    >Debug tools now have 1, 10, and 100 year aging.

--- Source/AgeTools.cs
+++ Source/AgeTools.cs
@@ -12,12 +12,22 @@ namespace Verse
     public static class MetamorphosisToolsPawn
     {
         [DebugAction("Pawns", null, actionType = DebugActionType.ToolMapForPawns, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        private static void AgePawn1Year(Pawn p)
+        {
+            p.ageTracker.AgeBiologicalTicks = p.ageTracker.AgeBiologicalTicks + 3600000;
+            p.ageTracker.AgeChronologicalTicks = p.ageTracker.AgeChronologicalTicks + 3600000;
+        }
+        [DebugAction("Pawns", null, actionType = DebugActionType.ToolMapForPawns, allowedGameStates = AllowedGameStates.PlayingOnMap)]
         private static void AgePawn10Years(Pawn p)
         {
             p.ageTracker.AgeBiologicalTicks = p.ageTracker.AgeBiologicalTicks + 36000000;
-            p.ageTracker.AgeChronologicalTicks = p.ageTracker.AgeChronologicalTicks + 36000000;
+            p.ageTracker.AgeChronologicalTicks = p.ageTracker.AgeChronologicalTicks + 360000000;
         }
-
+        [DebugAction("Pawns", null, actionType = DebugActionType.ToolMapForPawns, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        private static void AgePawn100Years(Pawn p)
+        {
+            p.ageTracker.AgeBiologicalTicks = p.ageTracker.AgeBiologicalTicks + 360000000;
+            p.ageTracker.AgeChronologicalTicks = p.ageTracker.AgeChronologicalTicks + 360000000;
+        }
     }
-
 }
