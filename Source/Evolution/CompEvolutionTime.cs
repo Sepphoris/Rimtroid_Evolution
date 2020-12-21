66e38071e6c07352582ecd8124caaae8ba126cb5
Drazzii <70376013+Drazzii@users.noreply.github.com>
2020-12-21 11:40:12 -0800

    Added Evolution Time

---
+++ Source/Evolution/CompEvolutionTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace RT_Rimtroid
+{
+	public class CompEvolutionTime : ThingComp
+	{
+		public CompProperties_EvolutionTime Props
+		{
+			get
+			{
+				return (CompProperties_EvolutionTime)this.props;
+			}
+		}
+
+		public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Values.Look<int>(ref this.evolutionTick, "evolutionTick", 0, false);
+		}
+
+
+		public override string CompInspectStringExtra()
+		{
+			int num = (int)(((float)this.rareTicksInAYear * this.Props.timeInYears - (float)this.evolutionTick) * 250f);
+			return TranslatorFormattedStringExtensions.Translate(this.Props.reportString, GenDate.ToStringTicksToPeriod(num, false, false, false, true));
+		}
+
+		public int evolutionTick;
+
+		public int rareTicksInAYear = 14400;
+	}
+}
---
+++ Source/Evolution/CompProperties_EvolutionTime.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+using System;
+
+namespace RT_Rimtroid
+{
+	public class CompProperties_EvolutionTime : CompProperties
+
+	{
+		public CompProperties_EvolutionTime()
+		{
+			this.compClass = typeof(CompEvolutionTime);
+		}
+
+		public float timeInYears;
+
+		public string reportString = "RT_TimeToEvolution";
+	}
+}
