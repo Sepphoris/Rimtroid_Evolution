697b46f92ecceb3960cda361bfdfb7a5e6cd3863
Taranchuk <nfhfyxer@gmail.com>
2020-12-23 16:32:13 +0200

    Revert "Reverted broken code"
    This reverts commit 16d5af003fe812315ac88534fb0eba0c1793ad71.

--- Source/Evolution/CompEvolutionTime.cs
+++ Source/Evolution/CompEvolutionTime.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using RimWorld;
+using UnityEngine;
 using Verse;
 using Verse.Sound;
 
@@ -21,11 +22,27 @@ namespace RT_Rimtroid
 			Scribe_Values.Look<int>(ref this.evolutionTick, "evolutionTick", 0, false);
 		}
 
+		public Pawn Metroid => this.parent as Pawn;
 
 		public override string CompInspectStringExtra()
 		{
-			int num = (int)(((float)this.rareTicksInAYear * this.Props.timeInYears - (float)this.evolutionTick) * 250f);
-			return TranslatorFormattedStringExtensions.Translate(this.Props.reportString, GenDate.ToStringTicksToPeriod(num, false, false, false, true));
+			for (var i = 0; i < Metroid.def.race.lifeStageAges.Count; i++)
+            {
+				Log.Message("Life: " + Metroid.def.race.lifeStageAges[i] + " - " + i);
+            }
+				
+			Log.Message("Metroid.ageTracker.CurLifeStageIndex: " + Metroid.ageTracker.CurLifeStageIndex);
+			if (Metroid.ageTracker.CurLifeStageIndex < Metroid.def.race.lifeStageAges.Count - 1)
+			{
+				float num4 = Metroid.def.race.lifeStageAges[Metroid.ageTracker.CurLifeStageIndex + 1].minAge - Metroid.ageTracker.AgeBiologicalYearsFloat;
+				int num5 = (Current.ProgramState == ProgramState.Playing) ? Find.TickManager.TicksGame : 0;
+				var nextLifeStageChangeTick = num5 + (long)Mathf.Ceil(num4 * 3600000f);
+				return ((int)nextLifeStageChangeTick).ToStringTicksToDays();
+			}
+			else
+			{
+				return "Max evolution";
+			}
 		}
 
 		public int evolutionTick;
--- Source/Harmony/Patches.cs
+++ Source/Harmony/Patches.cs
@@ -50,50 +50,66 @@ namespace RT_Rimtroid
         }
     }
 
+    [HarmonyPatch(typeof(Pawn), "SpawnSetup")]
+    public static class SpawnSetup_Patch
+    {
+        public static bool Prefix(Pawn __instance)
+        {
+            if (GenDate.DaysPassed < 3)
+            {
+                if (__instance.def == RT_DefOf.RT_GammaMetroid || __instance.def == RT_DefOf.RT_ZetaMetroid || __instance.def == RT_DefOf.RT_OmegaMetroid)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
     //[HarmonyPatch(typeof(Pawn), "Kill")]
     //public static class RT_Desiccator_Pawn_Kill_Patch
     //{
-        //[HarmonyPostfix]
-        //public static void Postfix(Pawn __instance, DamageInfo? dinfo, Hediff exactCulprit)
-        //{
-            //if (dinfo.HasValue)
-            //{
-                //if (dinfo.Value.Instigator != null)
-                //{
-                    //Thing inst = dinfo.Value.Instigator;
-                    //RT_DesiccatorExt desiccator = inst.def.GetModExtension<RT_DesiccatorExt>();
-                    //if (desiccator != null)
-                    //{
-                        //if (desiccator.RT_DesiccatedDef != null)
-                        //{
-                            //FieldInfo corpse = typeof(Pawn).GetField("Corpse", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField);
-                            //Traverse.Create(__instance);
-                            //corpse.SetValue(__instance, ThingMaker.MakeThing(desiccator.RT_DesiccatedDef));
-                        //}
-                        //else
-                        //{
-                            //CompRottable compRottable = __instance.Corpse.TryGetComp<CompRottable>();
-                            //compRottable.RotImmediately();
-                       // }
-                    //}
-                //}
-            //}
-            //HediffDef def = DefDatabase<HediffDef>.GetNamed("RT_LifeDrainSickness");
-            //if (__instance.health.hediffSet.HasHediff(def))
-            //{
-                //CompRottable compRottable = __instance.Corpse.TryGetComp<CompRottable>();
-                //compRottable.RotImmediately();
-            //}
-            //if (__instance.Corpse.GetRotStage() == RotStage.Fresh)
-            //{
-                //Log.Message(__instance + " failed rot");
-            //}
-            /*
-            else
-            {
-                Log.Message(__instance + " rotted by");
-            }
-            */
-        //}
+    //[HarmonyPostfix]
+    //public static void Postfix(Pawn __instance, DamageInfo? dinfo, Hediff exactCulprit)
+    //{
+    //if (dinfo.HasValue)
+    //{
+    //if (dinfo.Value.Instigator != null)
+    //{
+    //Thing inst = dinfo.Value.Instigator;
+    //RT_DesiccatorExt desiccator = inst.def.GetModExtension<RT_DesiccatorExt>();
+    //if (desiccator != null)
+    //{
+    //if (desiccator.RT_DesiccatedDef != null)
+    //{
+    //FieldInfo corpse = typeof(Pawn).GetField("Corpse", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField);
+    //Traverse.Create(__instance);
+    //corpse.SetValue(__instance, ThingMaker.MakeThing(desiccator.RT_DesiccatedDef));
+    //}
+    //else
+    //{
+    //CompRottable compRottable = __instance.Corpse.TryGetComp<CompRottable>();
+    //compRottable.RotImmediately();
+    // }
+    //}
+    //}
+    //}
+    //HediffDef def = DefDatabase<HediffDef>.GetNamed("RT_LifeDrainSickness");
+    //if (__instance.health.hediffSet.HasHediff(def))
+    //{
+    //CompRottable compRottable = __instance.Corpse.TryGetComp<CompRottable>();
+    //compRottable.RotImmediately();
+    //}
+    //if (__instance.Corpse.GetRotStage() == RotStage.Fresh)
+    //{
+    //Log.Message(__instance + " failed rot");
+    //}
+    /*
+    else
+    {
+        Log.Message(__instance + " rotted by");
+    }
+    */
+    //}
     //}
 }
